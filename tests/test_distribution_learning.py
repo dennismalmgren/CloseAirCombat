28b2eb4c6d7ac1e0c51e135986b07cf791056985
Dennis Malmgren <dennis@dennismalmgren.se>
2024-04-22 12:06:00 +0200

    Added

--- tests/test_distribution_learning.py
+++ tests/test_distribution_learning.py
@@ -6,10 +6,11 @@ import torch
 import math
 from tensordict.nn import TensorDictModule, TensorDictSequential
 from tensordict import TensorDict
+import matplotlib.pyplot as plt
 
 qvalue_net_kwargs = {
-        "num_cells": [256, 256],
-        "out_features": 1,
+        "num_cells": [512, 512],
+        "out_features": 90,
         "activation_class": torch.nn.ReLU,
     }
 
@@ -31,18 +32,56 @@ qvalue = TensorDictSequential(qvalue1)
 optim = torch.optim.Adam(qvalue.parameters(), lr=1e-3)
 
 in_data = torch.tensor([0.0])
-label_generator = torch.distributions.Normal(0.0, 1)
+label_generator_1 = torch.distributions.Normal(-1.0, 0.5)
+label_generator_2 = torch.distributions.Normal(1.0, 0.5)
 in_data = torch.tensor([0.0]).expand((256, 1))
 in_data_td = TensorDict({"observation": in_data}, batch_size=256)
+Vmin = -4.0
+Vmax = 4.0
+nbins = 90
+support = torch.linspace(Vmin, Vmax, nbins)
+atoms = support.numel()
+Vmin = support.min()
+Vmax = support.max()
+delta_z = (Vmax - Vmin) / (atoms - 1)
+stddev = 0.75 * delta_z
+support_plus = support + delta_z / 2
+support_minus = support - delta_z / 2
+
+
 for i in range(10000):
-    label = label_generator.sample((256,))
+    label_1 = label_generator_1.sample((128,))
+    label_2 = label_generator_2.sample((128,))
+    label = torch.cat([label_1, label_2], dim=0)
+    label = label[torch.randperm(label.size(0))]
+    label = label.unsqueeze(-1)
+    dist = torch.distributions.Normal(label, stddev)
+    cdf_plus = dist.cdf(support_plus)
+    cdf_minus = dist.cdf(support_minus)
+    m = cdf_plus - cdf_minus
+    #m[..., 0] = cdf_plus[..., 0]
+    #m[..., -1] = 1 - cdf_minus[..., -1]
+    m = m / m.sum(dim=-1, keepdim=True) 
+
+    #randomize order
     optim.zero_grad()
     out = qvalue(in_data_td)
-    loss = torch.nn.functional.mse_loss(out["state_action_value"].mean(-1), label)
+#    loss = torch.nn.functional.mse_loss(out["state_action_value"].mean(-1), label)
+    loss = torch.nn.functional.cross_entropy(out["state_action_value"], m)
     loss.backward()
     optim.step()
     if i % 100 == 0:
         print("Loss: ", loss.item())
-        print("Out: ", out["state_action_value"].mean())
+        mean_pred = out["state_action_value"].softmax(-1) @ support
+        print("Mean Pred: ", mean_pred.mean())
+        print("Out: ", out["state_action_value"].softmax(-1).mean(0))
+        #print("Sorted: ", out["state_action_value"].softmax(-1).mean(0).argsort())
         #print("Label: ", label)
         print("")
+
+print("Final results")
+print("Mean Pred: ", mean_pred.mean())
+print("Out: ", out["state_action_value"].softmax(-1).mean(0))
+print("Support: ", support)
+plt.plot(support, out["state_action_value"].softmax(-1).mean(0).detach().numpy())
+plt.show()
