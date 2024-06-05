05a64e09c2e0b3a26bf0285a6d0fb4c2f3aa46de
Dennis Malmgren <dennis@dennismalmgren.se>
2024-06-05 07:37:33 +0200

    added

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --gpus 1
+#SBATCH --reservation=1g.10gb
 #SBATCH -t 05:00:00
 #SBATCH -o runs/%j.out
 
@@ -25,4 +25,5 @@ done
 # image is located here
 cd /proj/berzelius-aiics-real/users/x_denma/
 
-apptainer exec --env "WANDB_API_KEY=f832ecbebaa081e6438201bd475fe26f9f0b1d82" --nv -B ./projs/CloseAirCombat:/app berzdev_latest.sif bash -c 'cd /app && python -m a2c_bandit_ce.a2c_mujoco'
+apptainer exec --env "WANDB_API_KEY=f832ecbebaa081e6438201bd475fe26f9f0b1d82" --nv -B ./projs/CloseAirCombat:/app berzdev_latest.sif bash -c "cd /app && python -m a2c_bandit_ce.a2c_mujoco logger.group_name=a2c_ce_64 logger.exp_name=a2c_ce_64 loss.loss_policy_type=cross_entropy"
+#apptainer exec --env "WANDB_API_KEY=f832ecbebaa081e6438201bd475fe26f9f0b1d82" --nv -B ./projs/CloseAirCombat:/app berzdev_latest.sif bash -c "cd /app && python -m a2c.a2c_mujoco"
