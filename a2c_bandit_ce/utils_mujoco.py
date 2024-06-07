4566a4bf1bd1413e48ccc172e4d222f9c8813402
dennismalmgren <dennis@dennismalmgren.se>
2024-06-07 08:59:18 +0200

    added

--- a2c_bandit_ce/utils_mujoco.py
+++ a2c_bandit_ce/utils_mujoco.py
@@ -18,6 +18,7 @@ from torchrl.envs import (
     StepCounter,
     TransformedEnv,
     VecNorm,
+    CatTensors
 )
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.modules import MLP, ProbabilisticActor, TanhNormal, ValueOperator
@@ -57,7 +58,9 @@ class SupportOperator(torch.nn.Module):
     def forward(self, x):
         x = x.reshape(*x.shape[:-1], *self.support.shape)
         return (x.softmax(-1) * self.support).sum(-1)
-    
+
+
+
 def make_ppo_models_state(proof_environment, cfg):
 
     # Define input shape
@@ -75,35 +78,46 @@ def make_ppo_models_state(proof_environment, cfg):
     nbins = cfg.network.nbins
     policy_supports = [torch.linspace(proof_environment.action_spec.space.low[i] * 5, proof_environment.action_spec.space.high[i] * 5, nbins) for i in range(num_outputs)]
     policy_support = torch.stack(policy_supports, dim=0)
+    policy_loc_keys = []
+    policy_loc_logit_keys = []
+    policy_modules = []
+    for a_ind in range(num_outputs):
+        # Define policy architecture
+        policy_network_1 = MLP(
+            in_features=input_shape[-1],
+            activation_class=torch.nn.Tanh,
+            out_features= nbins,  # predict only loc, for a single a_dim
+            num_cells=cfg.network.policy_hidden_sizes,
+            # norm_class=torch.nn.LayerNorm,
+            # norm_kwargs=[{"elementwise_affine": False,
+            #              "normalized_shape": hidden_size} for hidden_size in cfg.network.policy_hidden_sizes],
+        )
 
-    # Define policy architecture
-    policy_network_1 = MLP(
-        in_features=input_shape[-1],
-        activation_class=torch.nn.Tanh,
-        out_features=num_outputs * nbins,  # predict only loc
-        num_cells=cfg.network.policy_hidden_sizes,
-        # norm_class=torch.nn.LayerNorm,
-        # norm_kwargs=[{"elementwise_affine": False,
-        #              "normalized_shape": hidden_size} for hidden_size in cfg.network.policy_hidden_sizes],
-    )
+        # Initialize policy weights
+        for layer in policy_network_1.modules():
+            if isinstance(layer, torch.nn.Linear):
+                torch.nn.init.orthogonal_(layer.weight, 1.0)
+                layer.bias.data.zero_()
 
-    # Initialize policy weights
-    for layer in policy_network_1.modules():
-        if isinstance(layer, torch.nn.Linear):
-            torch.nn.init.orthogonal_(layer.weight, 1.0)
-            layer.bias.data.zero_()
+        policy_network_1_module = TensorDictModule(
+            module=policy_network_1,
+            in_keys=["observation"],
+            out_keys=["loc_logits_" + str(a_ind)]
+        )
 
-    policy_network_1_module = TensorDictModule(
-        module=policy_network_1,
-        in_keys=["observation"],
-        out_keys=["loc_logits"]
-    )
+        policy_loc_logit_keys.append("loc_logits_" + str(a_ind))
+        policy_modules.append(policy_network_1_module)
+    
+    logits_cat = CatTensors(in_keys=policy_loc_logit_keys, out_key="loc_logits")
+    
+
     policy_network_2 = SupportOperator(policy_support)
     policy_network_2_module = TensorDictModule(
-        module=policy_network_2,
-        in_keys=["loc_logits"],
-        out_keys=["loc"]
-    )
+            module=policy_network_2,
+            in_keys=["loc_logits"],
+            out_keys=["loc"]
+        )
+    policy_modules.append(logits_cat)
 
     policy_network_3 = AddStateIndependentNormalScale(proof_environment.action_spec.shape[-1])
     policy_network_3_module = TensorDictModule(
@@ -112,13 +126,12 @@ def make_ppo_models_state(proof_environment, cfg):
         out_keys=["loc", "scale"]
     )
     
+    policy_modules.append(policy_network_2_module)
+    policy_modules.append(policy_network_3_module)
 
     # Add probabilistic sampling of the actions
     policy_module = ProbabilisticActor(
-        TensorDictSequential(
-            policy_network_1_module, 
-            policy_network_2_module,
-            policy_network_3_module
+        TensorDictSequential(*policy_modules
         ),
         in_keys=["loc", "scale"],
         spec=CompositeSpec(action=proof_environment.action_spec),
@@ -149,6 +162,98 @@ def make_ppo_models_state(proof_environment, cfg):
     )
 
     return policy_module, value_module, policy_support
+
+# def make_ppo_models_state(proof_environment, cfg):
+
+#     # Define input shape
+#     input_shape = proof_environment.observation_spec["observation"].shape
+
+#     # Define policy output distribution class
+#     num_outputs = proof_environment.action_spec.shape[-1]
+#     distribution_class = TanhNormal
+#     distribution_kwargs = {
+#         "min": proof_environment.action_spec.space.low,
+#         "max": proof_environment.action_spec.space.high,
+#         "tanh_loc": False,
+#     }
+
+#     nbins = cfg.network.nbins
+#     policy_supports = [torch.linspace(proof_environment.action_spec.space.low[i] * 5, proof_environment.action_spec.space.high[i] * 5, nbins) for i in range(num_outputs)]
+#     policy_support = torch.stack(policy_supports, dim=0)
+
+#     # Define policy architecture
+#     policy_network_1 = MLP(
+#         in_features=input_shape[-1],
+#         activation_class=torch.nn.Tanh,
+#         out_features=num_outputs * nbins,  # predict only loc
+#         num_cells=cfg.network.policy_hidden_sizes,
+#         # norm_class=torch.nn.LayerNorm,
+#         # norm_kwargs=[{"elementwise_affine": False,
+#         #              "normalized_shape": hidden_size} for hidden_size in cfg.network.policy_hidden_sizes],
+#     )
+
+#     # Initialize policy weights
+#     for layer in policy_network_1.modules():
+#         if isinstance(layer, torch.nn.Linear):
+#             torch.nn.init.orthogonal_(layer.weight, 1.0)
+#             layer.bias.data.zero_()
+
+#     policy_network_1_module = TensorDictModule(
+#         module=policy_network_1,
+#         in_keys=["observation"],
+#         out_keys=["loc_logits"]
+#     )
+#     policy_network_2 = SupportOperator(policy_support)
+#     policy_network_2_module = TensorDictModule(
+#         module=policy_network_2,
+#         in_keys=["loc_logits"],
+#         out_keys=["loc"]
+#     )
+
+#     policy_network_3 = AddStateIndependentNormalScale(proof_environment.action_spec.shape[-1])
+#     policy_network_3_module = TensorDictModule(
+#         module=policy_network_3,
+#         in_keys=["loc"],
+#         out_keys=["loc", "scale"]
+#     )
+    
+
+#     # Add probabilistic sampling of the actions
+#     policy_module = ProbabilisticActor(
+#         TensorDictSequential(
+#             policy_network_1_module, 
+#             policy_network_2_module,
+#             policy_network_3_module
+#         ),
+#         in_keys=["loc", "scale"],
+#         spec=CompositeSpec(action=proof_environment.action_spec),
+#         distribution_class=distribution_class,
+#         distribution_kwargs=distribution_kwargs,
+#         return_log_prob=True,
+#         default_interaction_type=ExplorationType.RANDOM,
+#     )
+
+#     # Define value architecture
+#     value_mlp = MLP(
+#         in_features=input_shape[-1],
+#         activation_class=torch.nn.Tanh,
+#         out_features=1,
+#         num_cells=[64, 64],
+#     )
+
+#     # Initialize value weights
+#     for layer in value_mlp.modules():
+#         if isinstance(layer, torch.nn.Linear):
+#             torch.nn.init.orthogonal_(layer.weight, 0.01)
+#             layer.bias.data.zero_()
+
+#     # Define value module
+#     value_module = ValueOperator(
+#         value_mlp,
+#         in_keys=["observation"],
+#     )
+
+#     return policy_module, value_module, policy_support
 
 
 def make_ppo_models(env_name, cfg):
